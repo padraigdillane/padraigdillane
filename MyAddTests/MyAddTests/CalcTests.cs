708f9275778182033aaec2ed523b3bc5e48e4637
padraigdillane <padraigdillane@gmail.com>
2016-07-27 12:22:04 +0100

    Add Calc

---
+++ MyAddTests/MyAddTests/CalcTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calc.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void WhenPassed1And1_Returns2()
+        {
+            //Arrange 
+            int num1 = 1;
+            int num2 = 1;
+
+            //Act
+            int sum = Calc.Add(num1, num2);
+
+            Assert.AreEqual(2, sum);
+        }
+    }
+}
